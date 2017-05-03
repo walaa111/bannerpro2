5bed36699372ef473e5aa86b10b680eee96ec600
walaa111 <walaagamil18@gmail.com>
2017-05-03 19:35:30 +0200

    Update index.js

--- bannerPro/www/js/index.js
+++ bannerPro/www/js/index.js
@@ -1,49 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function adSetter(){
+alert(navigator.userAgent);
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-6869992474017983/9375997553',
+        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-6869992474017983/4806197152',
+        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-6869992474017983/8878394753',
+        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+    };
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+if(AdMob) AdMob.createBanner( {
+    isTesting:true, //Remove this Before publishing your app
+    adId:admobid.banner, 
+    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+    autoShow:true} );
 
-        console.log('Received Event: ' + id);
-    }
-};
+}
+  function onDeviceReady(){
+  alert("device ready");
+      adSetter();
+  }
+
+
+function domLoaded(){
+ document.addEventListener("deviceready", onDeviceReady, false);
+}
